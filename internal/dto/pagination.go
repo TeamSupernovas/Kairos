0f893aae98461c0b6d1b208bc723b58a1558c91c
neeharikasinghsjsu <143756330+neeharikasinghsjsu@users.noreply.github.com>
2024-12-02 20:26:43 -0800

    Delete internal directory

--- internal/dto/pagination.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type PaginatedResponse[T any] struct {
-	TotalCount		int		`json:"totalCount"`
-	TotalPages		int		`json:"totalPages"`
-	Page			int		`json:"page"`
-	PageSize		int		`json:"pageSize"`
-	Data[]			[]T		`json:"data"`
-}
--- internal/handler/image_handler.go
+++
@@ -1,5 +0,0 @@
-package handler
-
-type ImageHandler struct {
-	
-}
