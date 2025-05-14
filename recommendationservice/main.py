7a86b1c733d36bc9e0bf4ac521e6da02b36516df
Neeharika Singh <neeharikasingh@Neeharikas-Air.attlocal.net>
2025-05-13 19:45:31 -0700

    cors

--- recommendationservice/main.py
+++ recommendationservice/main.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List, Optional
 from recommender.hybrid_recommender import hybrid_recommend
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class DishInput(BaseModel):
     DishID: str
